00a99d0d5d0bddeb0658c287ef4597b93941210a
quangminh <tomminhquan1411@gmail.com>
2024-10-18 14:24:40 +0700

    ProgressBar,MonitorBar,ScrollBar, SlideBar

---
+++ src/main/java/com/quangminh/chapter6/SliderExample.java
@@ -0,0 +1,32 @@
+package com.quangminh.chapter6;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SliderExample extends JPanel {
+    public SliderExample() {
+
+        super(true);
+        this.setLayout(new BorderLayout());
+        JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 25);
+
+        slider.setMinorTickSpacing(2);
+        slider.setMajorTickSpacing(10);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+
+        // We'll just use the standard numeric labels for now...
+        slider.setLabelTable(slider.createStandardLabels(10));
+
+        add(slider, BorderLayout.CENTER);
+    }
+
+    public static void main(String s[]) {
+        JFrame frame = new JFrame("Slider Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new SliderExample());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+}
